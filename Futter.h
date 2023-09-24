edd3722c833e9cbb7b15644874ad8a76cff03a4a
Kofi <kofi_offei@icloud.com>
2023-09-24 08:11:26 +0200

    Revert "docs: add comments"
    This reverts commit 2449759f1854ee1e82ceb6832596037a24554b8b.

--- Futter.h
+++ Futter.h
@@ -3,73 +3,60 @@
 
 #include <random>
 #include <iostream>
-#include "ic.hpp"  // Include necessary header files
+#include "ic.hpp"
 
 class Futter
 {
 public:
-	// Constructor to initialize Futter object
 	Futter(short GameMinXPos, short GameMaxXPos, short GameMinYPos, short GameMaxYPos);
-
-	// Destructor
 	~Futter();
 
-	// Display the Futter on the screen
 	void DisplayFutter() const;
-
-	// Remove the Futter from the screen
 	void RemoveFutter();
 
-	// Get the X position of the Futter
 	short GetFutterXPos() const;
-
-	// Get the Y position of the Futter
 	short GetFutterYPos() const;
 
-	// Initialize the position of the Futter while avoiding the player's position
 	void initializeFutterPos(short PlayerXPos, short PlayerYPos);
-
 private:
-	short Xpos;        // X position of the Futter
-	short Ypos;        // Y position of the Futter
+	short Xpos;
+	short Ypos;
 
-	short MinXPos, MaxXPos;  // Minimum and maximum X positions for the Futter
-	short MinYPos, MaxYPos;  // Minimum and maximum Y positions for the Futter
+	short MinXPos, MaxXPos;
+	short MinYPos, MaxYPos;
 };
 
 Futter::Futter(short GameMinXPos, short GameMaxXPos, short GameMinYPos, short GameMaxYPos)
 {
-	// Initialize the minimum and maximum positions for the Futter
-	MinXPos = GameMinXPos + 1;
-	MaxXPos = GameMaxXPos - 1;
-	MinYPos = GameMinYPos + 1;
-	MaxYPos = GameMaxYPos - 1;
+	MinXPos = GameMinXPos+1;
+	MaxXPos = GameMaxXPos -1;
+
+	MinYPos = GameMinYPos+1;
+	MaxYPos = GameMaxYPos -1;
 }
 
 void Futter::DisplayFutter() const
 {
-	// Display the Futter symbol at its current position
 	gotoxy(Xpos, Ypos);
 	std::cout << (char)254;
 }
 
 void Futter::RemoveFutter()
 {
-	// Remove the Futter symbol from the screen by printing a space at its position
 	gotoxy(Xpos, Ypos);
 	std::cout << ' ';
 }
 
 void Futter::initializeFutterPos(short PlayerXPos, short PlayerYPos)
 {
-	// Initialize the position of the Futter while avoiding the player's position
 	std::default_random_engine XRandom(time(0));
 	std::uniform_int_distribution<int> Xbereich(MinXPos, MaxXPos);
 
 	std::default_random_engine YRandom(time(0));
 	std::uniform_int_distribution<int> Ybereich(MinYPos, MaxYPos);
 
-	do {
+	do{
+
 		Xpos = Xbereich(XRandom);
 		Ypos = Ybereich(YRandom);
 	} while (Xpos == PlayerXPos && Ypos == PlayerYPos);
@@ -78,9 +65,10 @@ void Futter::initializeFutterPos(short PlayerXPos, short PlayerYPos)
 short Futter::GetFutterXPos() const { return Xpos; }
 short Futter::GetFutterYPos() const { return Ypos; }
 
+
 Futter::~Futter()
 {
-	// Destructor implementation (if needed)
+
 }
 
 #endif
