1f2112f8d98048f8b506c938981a903cdea339c9
Kofi <kofi_offei@icloud.com>
2023-09-24 08:11:13 +0200

    Revert "docs: add comments"
    This reverts commit cc5f4c89d99d5eaeb0c0df966d96b28035ed7083.

--- Snake_Player.h
+++ Snake_Player.h
@@ -1,11 +1,11 @@
 #ifndef SNAKEPPLAYER_HPP
 #define SNAKEPLAYER_HPP
-
 #include <map>
 #include <string>
 #include <iomanip>
 #include <iostream>
 #include "ic.hpp"
+
 
 using namespace ic;
 using namespace ic::shorties;
@@ -13,136 +13,284 @@ using namespace ic::shorties;
 class Snake_Player
 {
 public:
-    // Constructor to initialize the snake
-    Snake_Player(short GMIX, short GMAX, short GMIY, short GMAY);
-    
-    // Destructor
-    ~Snake_Player();
+	// Snake intialisieren
+	Snake_Player(short GMIX, short GMAX, short GMIY, short GMAY);
+	~Snake_Player();
 
-    // Draw the snake on the screen
-    void Draw_Snake();
-    
-    // Remove the snake from the screen
-    void Remove_Draw();
+	// Snake ausgeben
+	void Draw_Snake();
+	void Remove_Draw();
 
-    // Move the snake up, down, left, and right
-    void Gehe_Hoch();
-    void Gehe_Runter();
-    void Gehe_Links();
-    void Gehe_Rechts();
+	// Snake Bewegungen
+	void Gehe_Hoch();
+	void Gehe_Runter();
+	void Gehe_Links();
+	void Gehe_Rechts();
 
-    // Get the position of the snake
-    short GetSnakePosX() const;
-    short GetSnakePosY() const;
+	// Position von Snake
+	short GetSnakePosX() const;
+	short GetSnakePosY() const;
 
-    // Check for collisions
-    bool kollision();
-
-    // Add a segment to the snake
-    void Add_Snake(int OBEN = 0, int UNTEN = 0, int RECHTS = 0, int LINKS = 0);
-
+	// Kollision testen
+	bool kollision();
+	
+	void Add_Snake(int OBEN, int UNTEN, int RECHTS, int LINKS);
 private:
-    // Game window properties
-    short GameWindowMinHeigth;
-    short GameWindowMaxWidth;
-    short GameWindowMaxHeigth;
-    short GameWindowMinWidth;
+	
+	// Gamefenster Eigenschaften
+	short GameWindowMinHeigth;
+	short GameWindowMaxWidth;
+	short GameWindowMaxHeigth;
+	short GameWindowMinWidth;
 
-    // Snake properties
-    int SnakeSize;
-    std::multimap<char, std::pair<short, short>> Snake;
+	// Schlange
+	int SnakeSize;
+	std::multimap<char, std::pair<short, short>> Snake;
 };
 
-// Constructor to initialize the snake
 Snake_Player::Snake_Player(short GMIX, short GMAX, short GMIY, short GMAY)
 {
-    SnakeSize = 0;
+	SnakeSize = 0;
 
-    GameWindowMinHeigth = GMIY;
-    GameWindowMaxWidth  = GMAX;
-    GameWindowMaxHeigth = GMAY;
-    GameWindowMinWidth  = GMIX;
-    
-    // Initialize the snake with default positions
-    Snake.insert({  '*', std::make_pair(GameWindowMaxWidth / 2, GameWindowMaxHeigth / 2) });
-    Snake.insert({  'O', std::make_pair(GameWindowMaxWidth / 2, GameWindowMaxHeigth / 2 - 1) });
-    Snake.insert({  'O', std::make_pair(GameWindowMaxWidth / 2, GameWindowMaxHeigth / 2 - 2) });
-    Snake.insert({  'O', std::make_pair(GameWindowMaxWidth / 2, GameWindowMaxHeigth / 2 - 3) });
+	GameWindowMinHeigth = GMIY;
+	GameWindowMaxWidth  = GMAX;
+	GameWindowMaxHeigth = GMAY;
+	GameWindowMinWidth  = GMIX;
+	
+	Snake.insert({  '*', std::make_pair(GameWindowMaxWidth / 2, GameWindowMaxHeigth / 2) });
+	Snake.insert({  'O', std::make_pair(GameWindowMaxWidth / 2, GameWindowMaxHeigth / 2 - 1) });
+	Snake.insert({  'O', std::make_pair(GameWindowMaxWidth / 2, GameWindowMaxHeigth / 2 - 2) });
+	Snake.insert({  'O', std::make_pair(GameWindowMaxWidth / 2, GameWindowMaxHeigth / 2 - 3) });
 }
 
-// Draw the snake on the screen
 void Snake_Player::Draw_Snake()
 {
-    for (auto Schlange : Snake)
-    {
-        gotoxy(Schlange.second.first, Schlange.second.second);
-        std::cout << Schlange.first;
-    }
+	for (auto Schlange : Snake)
+	{
+		gotoxy(Schlange.second.first, Schlange.second.second);
+		std::cout << Schlange.first;
+	}
+
 }
 
-// Remove the snake from the screen
 void Snake_Player::Remove_Draw()
 {
-    for (auto Schlange : Snake)
-    {
-        gotoxy(Schlange.second.first, Schlange.second.second);
-        std::cout << ' ';
-    }
+	for (auto Schlange : Snake)
+	{
+		gotoxy(Schlange.second.first, Schlange.second.second);
+		std::cout << ' ';
+	}
 }
 
-// Move the snake up
 void Snake_Player::Gehe_Hoch()
 {
-    // Logic to move the snake up
+	short OldPostempX = Snake.begin()->second.first, OldPostempY = Snake.begin()->second.second;
+	short NewPostempX, NewPostempY;
+
+	short i = 2;
+	bool frun = true;
+	for (auto& Schlange : Snake)
+	{
+		if (frun)
+		{
+			frun = false;
+		}
+		else
+		{
+			if (i % 2 == 0)
+			{
+				NewPostempX = Schlange.second.first;
+				NewPostempY = Schlange.second.second;
+
+				Schlange.second.first = OldPostempX;
+				Schlange.second.second = OldPostempY;
+			}
+			else if (i % 2 != 0)
+			{
+				OldPostempX = Schlange.second.first;
+				OldPostempY = Schlange.second.second;
+
+				Schlange.second.first = NewPostempX;
+				Schlange.second.second = NewPostempY;
+			}
+			++i;
+
+		}
+	}
+
+	--Snake.begin()->second.second;
 }
 
-// Move the snake left
 void Snake_Player::Gehe_Links()
 {
-    // Logic to move the snake left
+	short OldPostempX = Snake.begin()->second.first, OldPostempY = Snake.begin()->second.second;
+	short NewPostempX, NewPostempY;
+
+	short i = 2;
+	bool frun = true;
+	for (auto& Schlange : Snake)
+	{
+		if (frun)
+		{
+			frun = false;
+		}
+		else
+		{
+			if (i % 2 == 0)
+			{
+				NewPostempX = Schlange.second.first;
+				NewPostempY = Schlange.second.second;
+
+				Schlange.second.first = OldPostempX;
+				Schlange.second.second = OldPostempY;
+			}
+			else if (i % 2 != 0)
+			{
+				OldPostempX = Schlange.second.first;
+				OldPostempY = Schlange.second.second;
+
+				Schlange.second.first = NewPostempX;
+				Schlange.second.second = NewPostempY;
+			}
+			++i;
+
+		}
+	}
+
+	--Snake.begin()->second.first;
 }
 
-// Move the snake right
 void Snake_Player::Gehe_Rechts()
 {
-    // Logic to move the snake right
+	
+	short OldPostempX = Snake.begin()->second.first, OldPostempY = Snake.begin()->second.second;
+	short NewPostempX, NewPostempY;
+
+	short i = 2;
+	bool frun = true;
+	for (auto& Schlange : Snake)
+	{
+		if (frun)
+		{
+			frun = false;
+		}
+		else
+		{
+			if (i % 2 == 0)
+			{
+				NewPostempX = Schlange.second.first;
+				NewPostempY = Schlange.second.second;
+
+				Schlange.second.first = OldPostempX;
+				Schlange.second.second = OldPostempY;
+			}
+			else if (i % 2 != 0)
+			{
+				OldPostempX = Schlange.second.first;
+				OldPostempY = Schlange.second.second;
+
+				Schlange.second.first = NewPostempX;
+				Schlange.second.second = NewPostempY;
+			}
+			++i;
+
+		}
+	}
+
+	++Snake.begin()->second.first;
+
 }
 
-// Move the snake down
 void Snake_Player::Gehe_Runter()
 {
-    // Logic to move the snake down
+	short OldPostempX = Snake.begin()->second.first, OldPostempY = Snake.begin()->second.second;
+	short NewPostempX, NewPostempY;
+
+	short i = 2;
+	bool frun = true;
+	for (auto& Schlange : Snake)
+	{
+		if (frun)
+		{
+			frun = false;
+		}
+		else
+		{
+			if (i % 2 == 0)
+			{
+				NewPostempX = Schlange.second.first;
+				NewPostempY = Schlange.second.second;
+
+				Schlange.second.first = OldPostempX;
+				Schlange.second.second = OldPostempY;
+			}
+			else if (i % 2 != 0)
+			{
+				OldPostempX = Schlange.second.first;
+				OldPostempY = Schlange.second.second;
+
+				Schlange.second.first = NewPostempX;
+				Schlange.second.second = NewPostempY;
+			}
+			++i;
+
+		}
+	}
+
+	++Snake.begin()->second.second;
 }
 
-// Check for collisions
 bool Snake_Player::kollision()
 {
-    // Logic to check for collisions
-    return false;
+	std::multimap<char, std::pair<short, short>> SnakeCopy = Snake;
+	for (auto Schlange : Snake)
+	{
+		auto it = SnakeCopy.find('O');
+		if (it != SnakeCopy.end())
+		{
+			if (it->second.first == Snake.begin()->second.first &&
+				it->second.second == Snake.begin()->second.second)
+			{
+				return true;
+			}
+			else
+			{
+				SnakeCopy.erase(it);
+			}
+		}
+	}
+	return false;
 }
 
-// Get the X position of the snake's head
-short Snake_Player::GetSnakePosX() const
-{
-    return Snake.begin()->second.first;
-}
+short Snake_Player::GetSnakePosX() const { return Snake.begin()->second.first;  }
+short Snake_Player::GetSnakePosY() const { return Snake.begin()->second.second; }
 
-// Get the Y position of the snake's head
-short Snake_Player::GetSnakePosY() const
-{
-    return Snake.begin()->second.second;
-}
-
-// Destructor
 Snake_Player::~Snake_Player()
 {
 
 }
 
-// Add a segment to the snake
-void Snake_Player::Add_Snake(int OBEN, int UNTEN, int RECHTS, int LINKS)
+void Snake_Player::Add_Snake(int OBEN = 0, int UNTEN = 0, int RECHTS = 0, int LINKS = 0)
 {
-    // Logic to add a segment to the snake
+	if (OBEN != 0 && UNTEN == 0 && RECHTS == 0 && LINKS == 0)
+	{
+		Snake.insert({ 'O', std::make_pair(Snake.end().operator--()->second.first, Snake.end().operator--()->second.second - 1) });
+
+	}
+	else if (OBEN == 0 && UNTEN != 0 && RECHTS == 0 && LINKS == 0)
+	{
+		Snake.insert({'O', std::make_pair(Snake.end().operator--()->second.first, Snake.end().operator--()->second.second + 1) });
+
+	}
+	else if (OBEN == 0 && UNTEN == 0 && RECHTS != 0 && LINKS == 0)
+	{
+		Snake.insert({ 'O', std::make_pair(Snake.end().operator--()->second.first + 1, Snake.end().operator--()->second.second) });
+
+	}
+	else if (OBEN == 0 && UNTEN == 0 && RECHTS == 0 && LINKS != 0)
+	{
+		Snake.insert({ 'O', std::make_pair(Snake.end().operator--()->second.first -1, Snake.end().operator--()->second.second - 1) });
+	}
 }
 
 #endif
